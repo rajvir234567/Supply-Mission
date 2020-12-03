b4d957425361ae9b86cdc342ef6ee32dbed96dd1
rajvir234567 <73382875+rajvir234567@users.noreply.github.com>
2020-12-03 13:21:36 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -15,48 +15,40 @@ function setup() {
 	createCanvas(800, 700);
 	rectMode(CENTER);
 	
-
 	packageSprite=createSprite(width/2, 80, 10,10);
-	packageSprite.addImage(packageIMG)
-	packageSprite.scale=0.2
+	packageSprite.addImage(packageIMG);
+	packageSprite.scale=0.2;
 
 	helicopterSprite=createSprite(width/2, 200, 10,10);
 	helicopterSprite.addImage(helicopterIMG)
 	helicopterSprite.scale=0.6
 
-	groundSprite=createSprite(width/2, height-35, width,10);
-	groundSprite.shapeColor=color(255)
-
-
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:true});
 	World.add(world, packageBody);
-	
 
 	//Create a Ground
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
  	World.add(world, ground);
 
-
-	Engine.run(engine);
-  
+	Engine.run(engine);  
 }
-
 
 function draw() {
   rectMode(CENTER);
   background(0);
-  packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y 
+  packageSprite.x= packageBody.position.x;
+  packageSprite.y= packageBody.position.y;
+  rect(ground.position.x,ground.position.y,width,10);
   drawSprites();
- 
 }
 
 function keyPressed() {
  if (keyCode === DOWN_ARROW) {
 	packageBody.y = packageSprite.y - 1;
 	packageSprite.y = packageSprite.y - 1;
+	Matter.Body.setStatic(packageBody, false);
   }
 }
